07652e6c8a9aced18b2190f332976e1780e2cf10
Vivek Akella <akellavivek1999@gmail.com>
2019-12-20 14:41:48 +0530

    code optimized (converted to OOP)

--- in.py
+++ in.py
@@ -11,18 +11,37 @@ pygame.display.set_caption('My First game')
 
 clock = pygame.time.Clock()
 
-x = 50
-y = 400
-width = 64
-height = 64
 
-vel = 5
-is_jump = False
-jump_count = 10
+class Player(object):
 
-left = False
-right = False
-walk_count = 0
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.is_jump = False
+        self.jump_count = 10
+        self.left = False
+        self.right = False
+        self.walk_count = 0
+
+    def draw(self, win):
+        win.blit(bg, (0, 0))
+
+        if self.walk_count + 4 > 27:
+            self.walk_count = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+        elif man.right:
+            win.blit(walkRight[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+        else:
+            win.blit(char, (self.x, self.y))
+            self.walk_count = 0
+
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
@@ -31,25 +50,11 @@ char = pygame.image.load('standing.png')
 
 
 def redraw_game_window():
-    global walk_count
-
-    win.blit(bg, (0, 0))
-
-    if walk_count + 4 > 27:
-        walk_count = 0
-
-    if left:
-        win.blit(walkLeft[walk_count//3], (x, y))
-        walk_count += 1
-    elif right:
-        win.blit(walkRight[walk_count//3], (x, y))
-        walk_count += 1
-    else:
-        win.blit(char, (x, y))
-        walk_count = 0
-
+    man.draw(win)
     pygame.display.update()
 
+
+man = Player(300, 410, 64, 64)
 
 run = True
 while run:
@@ -60,22 +65,22 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-        left = True
-        right = False
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
 
-    elif keys[pygame.K_RIGHT] and x < screen_width - width:
-        x += vel
-        right = True
-        left = False
+    elif keys[pygame.K_RIGHT] and man.x < screen_width - man.width:
+        man.x += man.vel
+        man.right = True
+        man.left = False
 
     else:
-        right = False
-        left = False
-        walk_count = 0
+        man.right = False
+        man.left = False
+        man.walk_count = 0
 
-    if not is_jump:
+    if not man.is_jump:
         # if keys[pygame.K_UP] and y > vel:
         #     y -= vel
         #
@@ -83,21 +88,21 @@ while run:
         #     y += vel
 
         if keys[pygame.K_SPACE]:
-            is_jump = True
-            left = False
-            right = False
-            walk_count = 0
+            man.is_jump = True
+            man.left = False
+            man.right = False
+            man.walk_count = 0
 
     else:
-        if jump_count >= -10:
-            if jump_count > 0:
-                y -= (jump_count ** 2) * 0.5
+        if man.jump_count >= -10:
+            if man.jump_count > 0:
+                man.y -= (man.jump_count ** 2) * 0.5
             else:
-                y += (jump_count ** 2) * 0.5
-            jump_count -= 1
+                man.y += (man.jump_count ** 2) * 0.5
+            man.jump_count -= 1
         else:
-            is_jump = False
-            jump_count = 10
+            man.is_jump = False
+            man.jump_count = 10
 
     redraw_game_window()
 
