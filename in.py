6df91942de4ac74350cc0bd8549595fac9eaa8c8
Vivek Akella <akellavivek1999@gmail.com>
2019-12-20 15:22:23 +0530

    jump changed to up arrow and space shoots bullets

--- in.py
+++ in.py
@@ -1,8 +1,6 @@
 import pygame
 pygame.init()
 
-# import psyco
-#  psyco.full()
 
 win = pygame.display.set_mode((500, 500))
 screen_width = 500
@@ -25,6 +23,8 @@ class Player(object):
         self.left = False
         self.right = False
         self.walk_count = 0
+        self.jump_count = 10
+        self.standing = True
 
     def draw(self, win):
         win.blit(bg, (0, 0))
@@ -32,15 +32,33 @@ class Player(object):
         if self.walk_count + 4 > 27:
             self.walk_count = 0
 
-        if self.left:
-            win.blit(walkLeft[self.walk_count // 3], (self.x, self.y))
-            self.walk_count += 1
-        elif man.right:
-            win.blit(walkRight[self.walk_count // 3], (self.x, self.y))
-            self.walk_count += 1
+        if not self.standing:
+            if self.left:
+                win.blit(walkLeft[self.walk_count // 3], (self.x, self.y))
+                self.walk_count += 1
+            elif man.right:
+                win.blit(walkRight[self.walk_count // 3], (self.x, self.y))
+                self.walk_count += 1
         else:
-            win.blit(char, (self.x, self.y))
+            # win.blit(char, (self.x, self.y))
+            if self.right:
+                win.blit(walkRight[0], (self.x, self.y))
+            else:
+                win.blit(walkLeft[0], (self.x, self.y))
             self.walk_count = 0
+
+
+class Projectile(object):
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
@@ -51,11 +69,13 @@ char = pygame.image.load('standing.png')
 
 def redraw_game_window():
     man.draw(win)
+    for bullet in bullets:
+        bullet.draw(win)
     pygame.display.update()
 
 
 man = Player(300, 410, 64, 64)
-
+bullets = []
 run = True
 while run:
     clock.tick(27)
@@ -63,21 +83,36 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    for bullet in bullets:
+        if 500 > bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(Projectile(man.x + man.width//2, round(man.y + man.height//2), 6, (0, 0, 0), facing))
 
     if keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
         man.left = True
         man.right = False
+        man.standing = False
 
     elif keys[pygame.K_RIGHT] and man.x < screen_width - man.width:
         man.x += man.vel
         man.right = True
         man.left = False
+        man.standing = False
 
     else:
-        man.right = False
-        man.left = False
+        man.standing = True
         man.walk_count = 0
 
     if not man.is_jump:
@@ -87,7 +122,7 @@ while run:
         # if keys[pygame.K_DOWN] and y < screen_height - height:
         #     y += vel
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             man.is_jump = True
             man.left = False
             man.right = False
